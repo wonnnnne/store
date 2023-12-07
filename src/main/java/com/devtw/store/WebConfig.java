249ecb337cb6043b05289899fbf36a5f9e36282f
wonnnnne <twlee9212@gmail.com>
2023-12-07 22:46:21 +0900

    인터셉터 - 로그, 로그인인증 인터셉터 추가

---
+++ src/main/java/com/devtw/store/WebConfig.java
@@ -0,0 +1,25 @@
+package com.devtw.store;
+
+import com.devtw.store.web.interceptor.LogInterceptor;
+import com.devtw.store.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/user/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+}
